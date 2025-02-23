4168b9bdac9cb4e3a32348cbf7c246436ac43d32
Likha Jacob <likha.jacob03@gmail.com>
2025-02-23 12:53:08 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -38,3 +38,33 @@ const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
 console.log(mgr1.getDetails());//Manager: John Smith, ID: 201, Department: IT, Salary: 8000, Team Size: 5
 
 console.log("Expected Bonus:", mgr1.calculateBonus());//Expected Bonus: 7200
+//Task 3 - Created Company Class
+
+class Company { //Creating class Company
+    constructor(name, employees) { //Constructor setting up specified properties
+        this.name = name;
+        this.employees = []; //Initializing empty employees array
+    };
+    
+    addEmployee(employee) { //Setting up addEmployee method
+        this.employees.push(employee); //.push() employee to the employees array
+    }
+
+    listEmployees() { //Setting up listEmployees method
+        this.employees.forEach(employee => {console.log(employee.getDetails())}); //console.log the getDetails method forEach employee in employees array
+    }
+
+    calculateTotalPayroll() { //Task 4 - Add a method calculateTotalPayroll() to the Company class
+        return this.employees.reduce((total, employee) => total + employee.calculateAnnualSalary(), 0); //Task 4 - returns the sum of all employee salaries (including managers)
+    }
+
+    promoteToManager (employee, teamSize) { //Task 5 - Add a method promoteToManager(employee, teamSize) in the Company class.
+        const index = this.employees.indexOf(employee); //Finding employee position in employees array as index
+        this.employees[index] = new Manager(employee.name, employee.id, employee.department, employee.salary, teamSize); //Creating a Manager with the properties of employee saved at index
+    }
+}
+
+const company = new Company("TechCorp"); //Test case
+company.addEmployee(emp1); //Adding emp1 using addEmployee method
+company.addEmployee(mgr1); //Adding mgr1 using addEmployee method
+company.listEmployees(); //Logging
